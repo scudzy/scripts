a96221b592620c35c60508af327981b2ea5f137f
scudzy <scudzy@icloud.com>
2021-01-17 21:24:21 +0800

    Updated 17 Jan 21-23

---
+++ ps1/vdi-debian.ps1
@@ -0,0 +1,1 @@
+VBoxManage.exe modifymedium disk "S:\VM\Debian10.6.0\Debian_10.6.0_VB_LinuxVMImages.com-disk002.vdi" --compact
---
+++ ps1/vhd-optimize.ps1
@@ -0,0 +1,1 @@
+wsl --shutdown && Optimize-VHD -Path C:\Users\scudz\AppData\Local\Packages\CanonicalGroupLimited.Ubuntu20.04onWindows_79rhkp1fndgsc\LocalState\ext4.vhdx -Mode Full
