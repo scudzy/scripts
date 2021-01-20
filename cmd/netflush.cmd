2f3fe21b604af0bf7d2426044a86686b5f436abc
scudzy <scudzy@icloud.com>
2021-01-20 19:34:00 +0800

    ps1 Updated batch2gdrive & ffmpegts

--- cmd/netflush.cmd
+++ cmd/netflush.cmd
@@ -1,9 +1,11 @@
 ipconfig /release
 ipconfig /flushdns
-ipconfig /renew
 netsh winsock reset
 netsh interface ipv4 reset
 netsh interface ipv6 reset
 netsh winsock reset catalog
 netsh int ipv4 reset reset.log
 netsh int ipv6 reset reset.log
+netsh winsock reset
+netsh int ip reset
+ipconfig /renew
--- ps1/ffmpegts.ps1
+++ ps1/ffmpegts.ps1
@@ -1,3 +1,3 @@
-ffmpeg -i "concat:intermediate.ts" -c copy "Brianna Beach - New Years Resolution.mp4"
-ffmpeg -i "concat:intermediate1.ts" -c copy "Bettie Bondage - Sleepfuck Mommy.mp4"
-ffmpeg -i "concat:intermediate2.ts" -c copy "Family Therapy Kitten Latenight - The Family Friend.mp4"
+ffmpeg -i "concat:intermediate.ts" -c copy "Kimi The Milf Mommy - Pov Sweetmom Jerkoff.mp4"
+ffmpeg -i "concat:intermediate1.ts" -c copy "Kelly Payne - Moms Intrigued by Sons Tiny Cock.mp4"
+ffmpeg -i "concat:intermediate2.ts" -c copy "ImMeganLive - Mom Finds Son Tied Up By Bully.mp4"
