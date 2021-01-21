3a5a1f7441b7ca4e3a3df22ffa96f4e18825ee8b
scudzy <scudzy@icloud.com>
2021-01-22 07:50:51 +0800

    Updated shell scripts from wsl2

---
+++ sh/symlink.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+mkdir -p ~/.local/share/fonts
+ln -s ~/dotfiles/.aliases ~/.aliases
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gemrc ~/.gemrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+sudo ln -s ~/dotfiles/etc/wsl.conf /etc/wsl.conf
+cp -i ~/dotfiles/.gitconfig.user ~/.gitconfig.user
+cp -r ~/dotfiles/.local/share/fonts/Meslo\ LG\ S\ Regular\ Nerd\ Font\ Complete.ttf ~/.local/share/fonts/
