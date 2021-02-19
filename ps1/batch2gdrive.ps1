673087cd31b775d685975d57513e7174017ceb3a
scudzy <scudzy@icloud.com>
2021-02-19 20:56:49 +0800

    Added SnappyDriver sync 19 Feb 20-56

--- ps1/batch2gdrive.ps1
+++ ps1/batch2gdrive.ps1
@@ -18,4 +18,6 @@ rclone --exclude-from=d:\files-exc.txt sync 'd:\Documents' 'G-Drive:/Garbage/Doc
 #rclone --exclude-from=d:\files-exc.txt sync 'd:\IconsPacks' 'G-Drive:/Garbage/IconsPacks' --transfers=8 --stats=1s --drive-chunk-size=128M --tpslimit=10 --tpslimit-burst=10 -u -P -v && Write-Color "G50 IconsPacks Sync Completed" -ForegroundColor Green -LinesAfter 1 || Write-Color "G50 IconsPacks Sync Failed" -ForegroundColor Red -LinesAfter 1
 # Github notes scripts
 rclone --exclude-from=d:\files-exc.txt sync 'd:\Github' 'G-Drive:/Garbage/Github' --transfers=8 --stats=1s --drive-chunk-size=128M --tpslimit=10 --tpslimit-burst=10 -u -P -v && Write-Color "G50 Github Sync Completed" -ForegroundColor Green -LinesAfter 1 || Write-Color "G50 Github Sync Failed" -ForegroundColor Red -LinesAfter 1
-Write-Color "Synchronization completed" -ForegroundColor Green -LinesAfter 1
+#SnappyDriverInstaller
+rclone --exclude-from=d:\files-exc.txt sync 'D:\SnappyDriverInstaller' 'G-Drive:/Garbage/Drivers/SnappyDriverInstaller' --transfers=8 --stats=1s --drive-chunk-size=128M --tpslimit=10 --tpslimit-burst=10 -u -P -v && Write-Color "G50 SnappyDriverInstaller Sync Completed" -ForegroundColor Green -LinesAfter 1 || Write-Color "G50 SnappyDriverInstaller Sync Failed" -ForegroundColor Red -LinesAfter 1
+Write-Color "Synchronization completed" -ForegroundColor Cyan -LinesAfter 1
