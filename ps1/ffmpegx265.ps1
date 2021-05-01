a048aec17c9086d8e5b4d235588bbc444b8befcb
scudzy <22201569+scudzy@users.noreply.github.com>
2021-05-01 11:44:51 +0800

    :heavy_plus_sign: ps1 script
    Convert x264 to x265

---
+++ ps1/ffmpegx265.ps1
@@ -0,0 +1,6 @@
+$originalVids = Get-ChildItem *.mp4 -Recurse
+
+foreach ($inputVid in $originalVids) {
+    $outputVid = [io.path]::ChangeExtension($inputVid.FullName, 'x265.mp4')
+    ffmpeg.exe -i $inputVid.FullName -s 1280x720 -c:v libx265 -vtag hvc1 -preset ultrafast -crf 25 -c:a aac -strict -2 -b:a 96k $outputVid
+}
