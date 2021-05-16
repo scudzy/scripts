63d918f3da4478bef6eb5b0c96a3af786f2f71e5
scudzy <22201569+scudzy@users.noreply.github.com>
2021-05-16 23:31:04 +0800

    :hammer: Resize video to 480p

--- ps1/ffmpegx265.ps1
+++ ps1/ffmpegx265.ps1
@@ -2,5 +2,5 @@ $originalVids = Get-ChildItem *.mp4 -Recurse
 
 foreach ($inputVid in $originalVids) {
     $outputVid = [io.path]::ChangeExtension($inputVid.FullName, 'x265.mp4')
-    ffmpeg.exe -i $inputVid.FullName -s 1280x720 -c:v libx265 -vtag hvc1 -preset ultrafast -crf 25 -c:a aac -strict -2 -b:a 96k $outputVid
+    ffmpeg.exe -i $inputVid.FullName -s 854x480 -c:v libx265 -vtag hvc1 -preset ultrafast -crf 25 -c:a aac -strict -2 -b:a 96k $outputVid
 }
