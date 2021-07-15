983816383c80b19ebf5b524c17f02f3628dfdaaa
scudzy <22201569+scudzy@users.noreply.github.com>
2021-07-15 21:23:52 +0800

    :hammer: Modify syntax

--- ps1/launch-stremio.ps1
+++ ps1/launch-stremio.ps1
@@ -1,5 +1,3 @@
 Start-Process -FilePath "cmd.exe" -ArgumentList  "/c D:\Apps\Portable\Jackett\jackett_launcher.bat"
 Start-Sleep -Seconds 5
-Start-Process -FilePath "D:\Apps\Portable\PimpMyStremio\PimpMyStremio.exe"
-Start-Sleep -Seconds 30
-Start-Process -FilePath "C:\Users\scudzy\AppData\Local\Programs\LNV\Stremio-4\stremio.exe"
+Start-Process -FilePath "D:\Apps\Portable\PimpMyStremio\PimpMyStremio.exe" && Start-Process -FilePath "C:\Users\scudzy\AppData\Local\Programs\LNV\Stremio-4\stremio.exe"
