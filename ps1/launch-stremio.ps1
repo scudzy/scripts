3da41934f382712fb35963f760bbf07e67a02a77
scudzy <22201569+scudzy@users.noreply.github.com>
2021-07-19 14:41:20 +0800

    :hammer: Add sonarr into scripts

--- ps1/launch-stremio.ps1
+++ ps1/launch-stremio.ps1
@@ -1,3 +1,5 @@
 Start-Process -FilePath "cmd.exe" -ArgumentList  "/c D:\Apps\Portable\Jackett\jackett_launcher.bat"
 Start-Sleep -Seconds 5
+Start-Process -FilePath "D:\Apps\Portable\Sonarr\Sonarr.exe"
+Start-Sleep -Seconds 10
 Start-Process -FilePath "D:\Apps\Portable\PimpMyStremio\PimpMyStremio.exe" && Start-Process -FilePath "C:\Users\scudzy\AppData\Local\Programs\LNV\Stremio-4\stremio.exe"
